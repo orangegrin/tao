9d576a68cb33a3c85e552e955ef9434f42240f53
henry <xbhuang1994@gmail.com>
2018-11-22 14:54:52 +0800

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var redis = require("redis"),
+  client = redis.createClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/stats', function (req, res, next) {
+  let key = req.query.key;
+  client.hvals(key, (err, vals) => {
+    let results = {}
+    results['cur'] = JSON.parse(vals.pop());
+    results['pre'] = JSON.parse(vals.pop());
+    res.send(results)
+  })
 });
 
 module.exports = router;
